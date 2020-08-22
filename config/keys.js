9a191187c25724a70b4f984d30a7cb3bdb0822ae
Mi-Za-N <mizanwebdeveloper@gmail.com>
2020-08-22 23:06:18 +0600

    Google Oauth backend setup

---
+++ config/keys.js
@@ -0,0 +1,20 @@
+module.exports = {
+  googleClientID:
+    "502410818711-0ps7iv4k9m937imonigvm1469ni5c2k8.apps.googleusercontent.com",
+  googleClientSecret: "qSnVZQbl3mZg - LNMaRgDXDHn",
+  mongoURI:
+    "mongodb + srv://Mizan:mizan12255889@cluster0.4uuba.mongodb.net/emaily?retryWrites=true&w=majority",
+  cookieKey: "somethingSuperSecret",
+};
+// mongodb + srv://Mizan:<password>@cluster0.4uuba.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+// mongodb + srv://Mizanrahman:mizan12255889@cluster0.4uuba.mongodb.net/emaily?retryWrites=true&w=majority
+
+// // keys.js - figure out what set of credentials to return
+// if (process.env.NODE_ENV === "production") {
+//   // we are in production - return the prod set of keys
+//   module.exports = require("./prod");
+// } else {
+//   // we are in development - return the dev keys!!!
+//   module.exports = require("./dev");
+// }
